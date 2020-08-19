31a2380baab3645a02b5430aedab34a69af8e550
Pritivi Chhabria Sachin <pritivischhabria@berkeley.edu>
2020-08-19 15:28:25 +0530

    Initial Commit

---
+++ Image Recog CoreML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Image Recog CoreML
+//
+//  Created by Pritivi S Chhabria on 8/19/20.
+//  Copyright © 2020 Chiffonier Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
